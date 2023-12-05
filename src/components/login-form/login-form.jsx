0e0a7e00042be766aba5c5a4336f137bcb5eede4
Aleksandrov96 <pashaalexandrov1996@gmail.com>
2023-12-06 01:08:55 +0300

    added validation

--- src/components/login-form/login-form.jsx
+++ src/components/login-form/login-form.jsx
@@ -3,19 +3,34 @@ import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import useYupValidationResolver from "../../hooks/useYupValidationResolver";
 import "./login-form.scss";
 
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Please enter valid email")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(12, "Password cannot exceed more than 12 characters")
+    .required("Password is required"),
+});
+
 function LoginForm() {
-  const { register, handleSubmit, formState, reset } = useForm({
+  const { register, handleSubmit, formState } = useForm({
     defaultValues: {
       email: "",
       password: "",
     },
+    resolver: useYupValidationResolver(schema),
   });
   const { errors } = formState;
 
   const onSubmit = (data) => {
-    reset();
+    
   };
 
   return (
